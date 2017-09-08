c32d3f19ef0d4530e5723ee7b589022bc2e8470a
f1ux1uxday <fluxpatron@gmail.com>
2017-09-07 23:47:13 -0500

    re-style header after font change

--- src/components/ld-header.js
+++ src/components/ld-header.js
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
     return (
         <div className='header'>
-          <h2 id='name' >LUCAS DAYTON</h2>
+          <h1 id='name' >LUCAS DAYTON</h1>
         </div>
     )
   }
