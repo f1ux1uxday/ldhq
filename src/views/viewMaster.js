27b2be90543908f6c19fc3b499319464c5ad9439
f1ux1uxday <fluxpatron@gmail.com>
2017-12-31 15:47:44 -0600

    created API Overview route for future APIs

--- src/views/viewMaster.js
+++ src/views/viewMaster.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import HomeView from './homeView';
 import ProjectView from './projectView'
 import BlogView from './blogView'
+import ApiView from './apiView'
 
 const ViewMaster = () => (
   <div id='layout'>
@@ -10,6 +11,7 @@ const ViewMaster = () => (
       <Route path='/' exact component={HomeView} />
       <Route path='/projects' exact component={ProjectView} />
       <Route path='/blog' exact component={BlogView} />
+      <Route path='/api' exact component={ApiView} />
     </Switch>
   </div>
 )
