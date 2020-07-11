171059212575c5b77a673f4ce370442b1ffa8984
f1ux1uxday <fluxpatron@gmail.com>
2020-07-11 09:21:20 -0500

    removed repl.it, removed tracking of other component's state

--- src/components/ld-contact.js
+++ src/components/ld-contact.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react'
 
 import gitHubLogo from '../assets/GitHub_Logo.png'
 import linkedInLogo from '../assets/icons8-LinkedIn-48.png'
-import replitLogo from '../assets/repl-logo-small.png'
 
 class Contact extends Component {
+    componentDidMount() {
+        this.props.getInitialState()
+    }
+
     contactToggle() {
         if (this.props.contact === 'off') {
             this.props.changeContact('on')
@@ -15,13 +18,10 @@ class Contact extends Component {
     }
 
     stateCheckContact() {
-        if (this.props.blog !== 'off' || this.props.work !== 'off') {
-            return null
-        }
         if (this.props.contact === 'off') {
             return (
-                <div className='contact' onClick={this.contactToggle.bind(this)}>
-                    <h4 id='contact-off'>
+                <div className='contact-link' onClick={this.contactToggle.bind(this)}>
+                    <h4 id='contact-link-text'>
                         CONTACT
                     </h4>
                 </div>
@@ -34,18 +34,18 @@ class Contact extends Component {
                         <h4 id='contact-mail'>
                             self[at]lucasdayton.com
                         </h4>
-                        <div id='logos'>
-                            <a href='https://github.com/f1ux1uxday' target='blank'>
+                        <div className='logos'>
+                            <a
+                                href='https://github.com/f1ux1uxday'
+                                target='blank'
+                            >
                                 <img src={gitHubLogo} className='icon' id='gh-icon' alt='n'></img>
                             </a>
-                            <a 
-                                href='https://www.linkedin.com/in/lucas-dayton-911109a2/'
+                            <a
+                                href='https://www.linkedin.com/in/lucas-a-dayton/'
                                 target='blank'
                             >
                                 <img src={linkedInLogo} className='icon' id='li-icon' alt='n'></img>
-                            </a>
-                            <a href='https://repl.it/@fluxpatron' target='blank'>
-                                <img src={replitLogo} className='icon' id='li-icon' alt='n'></img>
                             </a>
                         </div>
                     </div>
