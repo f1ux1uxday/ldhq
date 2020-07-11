2e95117f5b0c002b9b521c2b0213e73991f418f1
f1ux1uxday <fluxpatron@gmail.com>
2020-07-11 09:22:48 -0500

    after refactor, no longer necessary to track state from other components

--- src/components/ld-work.js
+++ src/components/ld-work.js
@@ -1,35 +1,13 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 
 import WorkDisplay from './ld-work-display'
 
 class Work extends Component {
 
-    workToggle() {
-        if (this.props.work === 'off') {
-            this.props.changeWork('on')
-            console.log(this.props.work)
-        }
-    }
-
-    stateCheckWork() {
-        if (this.props.blog !== 'off') {
-            return null
-        }
-        if (this.props.work === 'off') {
-            return (
-                <Link to='/projects'>
-                    <div className='work' onClick={this.workToggle.bind(this)}>
-                        <h4 id='work-text'>
-                            PROJECTS
-                        </h4>
-                    </div>
-                </Link>
-            )
-        }
-        if (this.props.work === 'on') {
-            return (
-                <div className='work-view' onClick={this.workToggle.bind(this)}>
+    render() {
+        return (
+            <div>
+                <div className='work-view'>
                     <h4 id='work-view-text'>
                         PROJECTS
                     </h4>
@@ -38,14 +16,6 @@ class Work extends Component {
                         getDescription={this.props.getDescription}
                     />
                 </div>
-            )
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                {this.stateCheckWork()}
             </div>
         )
     }
